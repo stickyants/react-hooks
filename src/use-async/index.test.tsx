ba693c5c32a2c29eb6bb8c695d9411a13716661c
Suhail Abood <suhail.abood@gmail.com>
2019-07-12 00:19:46 +0100

    add useDebouncedDOMEvent

--- src/use-async/index.test.tsx
+++ src/use-async/index.test.tsx
@@ -4,41 +4,59 @@ import { useAsync } from '.';
 describe('use-async', () => {
   test('should not change view to error or success when cancelled', () => {
     const fn = jest.fn(() => new Promise(r => setTimeout(r, 5000)));
+
     const view = () => 'loading';
     const viewSuccess = () => 'success';
     const viewError = () => 'error';
+
     const { unmount, result } = renderHook(() => useAsync(fn, [], view, viewSuccess, viewError));
+
     expect(result.current).toBe('loading');
+
     unmount();
+
     expect(result.current).toBe('loading');
   });
 
   test('should change view to success when succeeds', async () => {
     const fn = jest.fn(() => new Promise(r => r()));
+
     const view = () => 'loading';
     const viewSuccess = () => 'success';
     const viewError = () => 'error';
     const { result } = renderHook(() => useAsync(fn, [], view, viewSuccess, viewError));
+
     const error = console.error;
     console.error = () => { };
+
     expect(result.current).toBe('loading');
+
     await new Promise(r => setTimeout(r));
+
     expect(result.current).toBe('success');
+
     console.error = error;
 
   });
 
   test('should change view to error when fails', async () => {
-    const error = console.error;
-    console.error = () => { };
     const fn = jest.fn(() => new Promise((_, r) => r()));
+
     const view = () => 'loading';
     const viewSuccess = () => 'success';
     const viewError = () => 'error';
+
     const { result } = renderHook(() => useAsync(fn, [], view, viewSuccess, viewError));
+
+    const error = console.error;
+    console.error = () => { };
+
     expect(result.current).toBe('loading');
+
     await new Promise(r => setTimeout(r));
+
     expect(result.current).toBe('error');
+
     console.error = error;
 
   });
---
+++ src/use-debounceddomevent/index.tsx
@@ -0,0 +1,17 @@
+import * as debounce from 'lodash.debounce';
+import { useDOMEvent } from '../use-domevent';
+import { useCallback } from 'react';
+
+export function useDebouncedDOMEvent(
+  ref: React.RefObject<HTMLElement>,
+  eventType: string,
+  callback: (e: Event) => void,
+  opts: AddEventListenerOptions = undefined,
+  debounceOptions: {
+    leading?: boolean,
+    trailing?: boolean,
+    maxWait?: number,
+  } = { trailing: true, maxWait: 250 }) {
+  const debouncedCallback = useCallback(debounce(callback, debounceOptions), []);
+  useDOMEvent(ref, eventType, debouncedCallback, opts);
+}
